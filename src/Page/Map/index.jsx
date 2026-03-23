193c7bdc165cedeff5596642e900dd70dd824fc8
vinhff-ff <nguyenducvinh7102006@gmail.com>
2026-03-23 23:34:24 +0700

    update

--- src/Page/Map/index.jsx
+++ src/Page/Map/index.jsx
@@ -490,6 +490,10 @@ const GearSVG = ({ className }) => (
 );
 
 function MapNode({ node, svgWidth, svgHeight }) {
+  const [hovered, setHovered] = useState(false);
+
+  const px = (node.x / 100) * svgWidth;
+  const py = (node.y / 100) * svgHeight;
 
   const nodeClass = [
     "node-btn",
